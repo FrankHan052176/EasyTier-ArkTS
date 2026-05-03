5d28fa24f32d25e151bcdd823637e2c178810376
FrankHan <hanjiale@hljit.edu.cn>
2026-05-04 03:51:14 +0800

    ai：modern dir

--- entry/src/main/ets/SchemaConfig.ts
+++
@@ -1,3 +0,0 @@
-export type ConfigJson = ESObject
-export type ConfigPrimitive = string | number | boolean | undefined | null
-export type ConfigFieldValue = ConfigPrimitive | string[] | ESObject[] | ESObject
